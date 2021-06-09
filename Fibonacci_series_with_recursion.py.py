d44a2d91c3d55dc05fbd1ceece2dc711e9a4b8c7
Dheeraj Kashyap <85519051+dheekash@users.noreply.github.com>
2021-06-10 03:30:20 +0530

    Update Fibonacci_series_with_recursion.py.py
    Your function fibo never returns a numerical value but only print messages if num is 0, 1 or 2. Hence fibo(num-1) and fibo(num-2) are NoneType. In addition to your prints you must return a value.

--- Fibonacci_series_with_recursion.py.py
+++ Fibonacci_series_with_recursion.py.py
@@ -2,21 +2,21 @@ def fibo(num):
 
     if num <= 0:
         print("Incoreect input number  should  be more than 0 or equal to 0.")
-    
+
     elif num == 1:
-        print("The series is 1.")
+        return 0
 
     elif num == 2:
-        print("The series is 1.")
-    
+        return 1
+
     else:
-        return(fibo(num-1) + fibo(num-2))   // error
+        return(fibo(num-1) + fibo(num-2))
 
 
 def main(): # fibonacci series  
-    
+
     num = int(input("Plz enter the nth  number to genrate fibbonaci numbers "))
-    
+
     print("The Fibonacci series is : ")
 
     for i in range(num):
